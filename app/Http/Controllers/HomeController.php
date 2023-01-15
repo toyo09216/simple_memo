07cf0251408dd9c1e16eac315214e76b718463d1
toyo09216 <atsushi09216@gmail.com>
2023-01-15 09:54:13 +0900

    view composerを用いて、共通処理をまとめる

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,19 +28,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // ここでメモを取得
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', Auth::id() )
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'DESC') //ASCが小さい順、DESCが大きい順
-            ->get();
-        
         $tags = Tag::where('user_id', '=', Auth::id())
             ->whereNull('deleted_at')
             ->orderBy('id', 'DESC')
             ->get();
 
-        return view('create', compact('memos', 'tags'));
+        return view('create', compact('tags'));
     }
 
     public function store(Request $request)
@@ -75,13 +68,6 @@ class HomeController extends Controller
 
     public function edit($id)
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', Auth::id() )
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'DESC') //ASCが小さい順、DESCが大きい順
-            ->get();
-            
-            
         $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
             ->leftjoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id' )
             ->leftjoin('tags', 'memo_tags.tag_id', '=', 'tags.id' )
@@ -100,7 +86,7 @@ class HomeController extends Controller
             ->orderBy('id', 'DESC')
             ->get();
         
-        return view('edit', compact('memos', 'edit_memo', 'include_tags', 'tags'));
+        return view('edit', compact('edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Memo;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 全てのメソッドが呼ばれる前に先に呼ばれるメソッド
+        view()->composer('*', function($view){
+            $memos = Memo::select('memos.*')
+                ->where('user_id', '=', Auth::id() )
+                ->whereNull('deleted_at')
+                ->orderBy('updated_at', 'DESC') //ASCが小さい順、DESCが大きい順
+                ->get();
+
+            $view->with('memos', $memos); // 第1引数はViewで使うときの命名、第2引数は渡したい変数or配列
+        });
     }
 
     /**
